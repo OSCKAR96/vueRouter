580c98d8bf549985934c8935fded7c8608cf0901
Oscar <oscarwebster96@gmail.com>
2024-10-17 23:49:18 -0300

    avanzando en el desafio, falta los props

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProductosView from '@/views/ProductosView.vue'
+import ContactosView from '@/views/ContactosView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/productos',
+    name: 'productos',
+    component: ProductosView
+  },
+  {
+    path: '/contactos',
+    name: 'contactos',
+    component: ContactosView
+  },
+  
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/ProductosView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>productos</h1>
+    </div>
+    <router-link to='/'>Volver inicio</router-link>
+</template>
+
+<style>
+body{
+    padding-top: 50px;
+}
+</style>
